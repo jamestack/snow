f2643aa9f0806c1ebf811cefe46c2c7c994b943f
james <james@github.com>
2020-11-23 05:34:02 +0800

    无限缓存的Channel

---
+++ channel.go
@@ -0,0 +1,156 @@
+package snow
+
+import (
+	"errors"
+	"sync"
+	"time"
+)
+
+// 无限缓存Channel
+type Channel struct {
+	lock sync.Mutex
+	nw   *sRing
+	nr   *sRing
+	close bool
+	cap int
+	maxCap int
+}
+
+type sRing struct {
+	value interface{}
+	next  *sRing
+}
+
+var ErrEmpty = errors.New("[Channel: Empty]")
+var ErrClosed = errors.New("[Channel: Closed]")
+
+// todo 动态设置最大容量
+//func (q *Channel) SetMaxSize(size uint) {
+//
+//}
+
+// 向Channel发送数据
+func (q *Channel) Send(v interface{}) (ok bool) {
+	q.lock.Lock()
+	defer q.lock.Unlock()
+
+	if q.close {
+		return false
+	}
+
+	if q.nw == nil {
+		head := &sRing{}
+		head.next = head
+		q.nw = head
+		q.nr = head
+		q.cap = 1
+	}
+
+	q.nw.value = v
+	next := q.nw.next
+	if next == q.nr {
+		list := make([]sRing, q.cap, q.cap)
+		for i:=0;i<q.cap-1;i++ {
+			list[i].next = &list[i+1]
+		}
+
+		list[q.cap-1].next = next
+		q.nw.next = &list[0]
+
+		next = &list[0]
+
+		q.cap += q.cap
+	}
+	q.nw = next
+	return true
+}
+
+// 阻塞直到取到值或者该队列关闭
+// @sleep 抢占式调度时的频率控制，达到任务或者线程优先级控制的目的
+func (q *Channel) Receive() (v interface{}, ok bool) {
+	for {
+		value,err := q.Get()
+		switch err {
+		case nil:
+			return value, true
+		case ErrClosed:
+			return nil, false
+		default:
+			<-time.After(1)
+		}
+	}
+}
+
+// 关闭队列并回收队列
+func (q *Channel) Close() (ok bool) {
+	q.lock.Lock()
+	defer q.lock.Unlock()
+
+	if q.close {
+		return false
+	}
+
+	// 更新关闭状态
+	q.close = true
+
+	// 回收内存
+	q.gc()
+
+	return true
+}
+
+// 从Chanel中取数据，不阻塞，如果队列为空或者已关闭则返回错误
+func (q *Channel) Get() (v interface{}, err error) {
+	q.lock.Lock()
+	defer q.lock.Unlock()
+
+	if q.nr == q.nw {
+		if q.close {
+			err = ErrClosed
+		}else {
+			err = ErrEmpty
+		}
+		return
+	}
+
+	v = q.nr.value
+	q.nr.value = nil
+	q.nr = q.nr.next
+
+	// 完全关闭后，自动回收内存
+	if q.close && q.nr == q.nw {
+		q.gc()
+	}
+	return
+}
+
+func (q *Channel) Peek() (v interface{}, err error) {
+	q.lock.Lock()
+	defer q.lock.Unlock()
+
+	if q.nr == q.nw {
+		if q.close {
+			err = ErrClosed
+		}else {
+			err = ErrEmpty
+		}
+		return
+	}
+
+	v = q.nr.value
+	return
+}
+
+// 内存回收，避免已close的queue占用大量内存无法被Runtime使用
+func (q *Channel) gc() {
+	if q.nw != nil && q.nr != nil {
+		var next *sRing
+		for n:=q.nw; n.next != q.nw && n.next != q.nr;n = n.next {
+			next = n.next
+			q.cap -= 1
+		}
+		if next != nil {
+			q.nw.next = next
+		}
+	}
+}
---
+++ example/channel_test.go
@@ -0,0 +1,141 @@
+package example
+
+import (
+	"fmt"
+	"snow"
+	"testing"
+	"time"
+)
+
+func TestChannel(t *testing.T) {
+	ch := &snow.Channel{}
+	//fmt.Println(ch.Close())
+
+	fmt.Println(ch.Get())
+	fmt.Println(ch.Send(1))
+	fmt.Println(ch.Send(2))
+	fmt.Println(ch.Get())
+	fmt.Println(ch.Get())
+	fmt.Println(ch.Get())
+	fmt.Println(ch.Send(3))
+	fmt.Println(ch.Send(4))
+	fmt.Println(ch.Send(5))
+	fmt.Println(ch.Send(6))
+	fmt.Println(ch.Close())
+	fmt.Println(ch.Get())
+	fmt.Println(ch.Get())
+	fmt.Println(ch.Get())
+	fmt.Println(ch.Get())
+	fmt.Println(ch.Get())
+
+}
+
+func TestChannelBench(t *testing.T) {
+	ch := snow.Channel{}
+
+	fmt.Println("send start", time.Now())
+	for i:=0;i<100000000;i++ {
+		ch.Send(i)
+	}
+	fmt.Println("send end", time.Now())
+
+	fmt.Println("get start", time.Now())
+	for i:=0;i<100000000;i++ {
+		ch.Get()
+	}
+	fmt.Println("get end", time.Now())
+
+	ch.Close()
+
+	fmt.Println("close end")
+
+	for {
+		_,ok := ch.Receive()
+		if !ok {
+			fmt.Println("closed")
+			break
+		}
+	}
+
+	fmt.Println("receive end")
+
+	<-time.After(30*time.Second)
+}
+
+func benchSysChanel(num int64) (res int64) {
+	ch := make(chan int64, 1024)
+	done := make(chan bool)
+	go func() {
+		for {
+			select {
+			case _,ok := <-ch:
+				if !ok {
+					done <- true
+					return
+				}
+
+				res += 1
+			}
+		}
+	}()
+	for i:=int64(1);i <= num;i++ {
+		ch <- i
+	}
+	close(ch)
+
+	<-done
+	return
+}
+
+func benchSnowChannel(num int64) (res int64) {
+	ch := snow.Channel{}
+	done := make(chan bool)
+
+	go func() {
+		for {
+			_,ok := ch.Receive()
+			if !ok {
+				done <- true
+				return
+			}
+
+			res += 1
+		}
+	}()
+
+	for i:=int64(1);i <= num;i++ {
+		ch.Send(i)
+	}
+	ch.Close()
+
+	<-done
+	return
+}
+
+func benchTwoChannel(num int64) {
+	fmt.Println("benchSysChanel start")
+	start := time.Now()
+	fmt.Println(benchSysChanel(num))
+	fmt.Println("benchSysChanel end", time.Now().Sub(start))
+
+	fmt.Println("benchSnowChannel start")
+	start = time.Now()
+	fmt.Println(benchSnowChannel(num))
+	fmt.Println("benchSnowChannel end", time.Now().Sub(start))
+}
+
+func TestBenchChanelChannel(t *testing.T) {
+	fmt.Println("-----------bench 1000 1k ---------")
+	benchTwoChannel(1000)
+	fmt.Println("-----------bench 100000 10w---------")
+	benchTwoChannel(100000)
+	fmt.Println("-----------bench 10000000 1000w ---------")
+	benchTwoChannel(10000000)
+	fmt.Println("-----------bench 100000000 1e ---------")
+	benchTwoChannel(100000000)
+	fmt.Println("-----------bench 1000000000 10e ---------")
+	benchTwoChannel(1000000000)
+
+
+	<-time.After(30 *time.Second)
+}
